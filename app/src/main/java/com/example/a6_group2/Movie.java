4a19a5ae81315d2fcba2b044700163eab856584d
“HongGuo” <guohong770@gmail.com>
2025-03-23 12:12:32 -0700

    movie card ui update

--- app/src/main/java/com/example/a6_group2/Movie.java
+++ app/src/main/java/com/example/a6_group2/Movie.java
@@ -3,9 +3,6 @@ package com.example.a6_group2;
 import com.google.gson.annotations.SerializedName;
 
 public class Movie {
-    @SerializedName("id")
-    private int id;
-    
     @SerializedName("title")
     private String title;
     
@@ -18,7 +15,6 @@ public class Movie {
     @SerializedName("vote_average")
     private double voteAverage;
 
-    public int getId() { return id; }
     public String getTitle() { return title; }
     public String getOverview() { return overview; }
     public String getPosterPath() { return posterPath; }
